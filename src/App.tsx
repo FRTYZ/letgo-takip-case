9d64415ac3d285d3fafd3493c0cdbbfd9680a2dd
Fırat Yıldız <firatyildiznet@gmail.com>
2023-12-18 19:05:10 +0300

    Added charts and rechart npm packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 
-import { Container, Grid, Button, Box } from '@mui/material'
+import { Container, Grid, Button, Box } from '@mui/material';
+
+import { PieChart, Pie, Cell, Tooltip } from "recharts";
 
 function App() {
+
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 }
+  ];
+  
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index
+  }: any) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
   return (
     <Container>
@@ -44,7 +82,23 @@ function App() {
       </Grid>
       <Grid container>
           <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                  <h1>Charts Area</h1>
+          <PieChart width={600} height={900} style={{ float: 'inline-end' }}>
+              <Pie
+                data={data}
+                cx={200}
+                cy={200}
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={200}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Tooltip />
+          </PieChart>
           </Grid>
       </Grid>
     </Container>
