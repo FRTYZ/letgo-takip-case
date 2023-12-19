c818cd045bebe542d2e9ffd5d592c46b1000c590
Fırat Yıldız <firatyildiznet@gmail.com>
2023-12-19 20:15:20 +0300

    Added updateCoinData for count key

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -14,20 +14,30 @@ const coinSlice = createSlice({
   
         if (existingIndex !== -1) {
           // Eğer öğe bulunursa, mevcut öğeyi güncelle
-          state.coinData[existingIndex] = action.payload;
+          state.coinData[existingIndex] = { ...state.coinData[existingIndex], ...action.payload };
         } else {
           // Eğer öğe bulunamazsa, yeni öğeyi ekle
           state.coinData.push(action.payload);
         }
-      },
+    },
     removeCoinBySymbol: (state, action) => {
         // Belirtilen sembol ismine sahip objeyi filtreleyerek yeni bir dizi oluştur
         state.coinData = state.coinData.filter((item) => item.symbol !== action.payload);
-    }
+    },
+    updateCoinData: (state, action: PayloadAction<{ symbol: string; count: number }>) => {
+        const { symbol, count } = action.payload;
+        const existingItem = state.coinData.find((item) => item.symbol === symbol);
+  
+        if (existingItem) {
+          // Eğer öğe bulunursa, sadece belirli alanları güncelle
+          existingItem.count = count;
+          // Eğer başka alanları da güncellemek istiyorsanız buraya ekleyebilirsiniz
+        }
+      },
   },
 });
 
-export const { setCoinData, removeCoinBySymbol } = coinSlice.actions;
+export const { setCoinData, removeCoinBySymbol, updateCoinData } = coinSlice.actions;
 
 // rootReducer oluşturulması ve tüm reducer'ların birleştirilmesi
 const rootReducer = {
